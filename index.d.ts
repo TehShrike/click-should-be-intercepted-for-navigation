629fd5948e2b25df88440584fb3abf90c7184ebc
Josh Duff <me@JoshDuff.com>
2021-12-22 12:54:54 -0600

    tabs wheee

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 declare module 'click-should-be-intercepted-for-navigation' {
-    declare function eventIsModifiedByKeyPress(event: Event): boolean;
-    declare function isLeftClick(event: Event): boolean;
+	declare function eventIsModifiedByKeyPress(event: Event): boolean;
+	declare function isLeftClick(event: Event): boolean;
 
-    function shouldInterceptEvent(event: Event): boolean;
-    export default shouldInterceptEvent;
+	function shouldInterceptEvent(event: Event): boolean;
+	export default shouldInterceptEvent;
 }
